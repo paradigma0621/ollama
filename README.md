4b4dffe425e9e0f986835d58dd744419fa830ba0
Lucas <paradigma0621@gmail.com>
2025-03-01 01:16:43 -0300

    Main struct of project

---
+++ README.md
@@ -0,0 +1,4 @@
+Demo project for SpringAI with Ollama
+
+Uses Java 17
+
---
+++ src/main/java/com/paradigma0621/ollama/OllamaApplication.java
@@ -0,0 +1,14 @@
+package com.paradigma0621.ollama;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OllamaApplication {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(OllamaApplication.class, args);
+	}
+
+}
